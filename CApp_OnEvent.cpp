e81bd46bb06ed692fa852a8da93f7907ea29f243
abisheksampath <sampath.ab@husky.neu.edu>
2018-03-19 01:41:46 -0400

    added OnKeyDown and OnExit code

--- CApp_OnEvent.cpp
+++ CApp_OnEvent.cpp
@@ -2,11 +2,41 @@
 
 void CApp::OnEvent(SDL_Event* event)
 {
-    if(event->type == SDL_QUIT) {
-        running = false;
-        return;
-    }
+//    if(event->type == SDL_QUIT) {
+//        running = false;
+//        return;
+//    }
+//
+//    // other events
+//    eventHandler->OnEvent(event);
+    CEvent::OnEvent(event);
+}
 
-    // other events
-    eventHandler->OnEvent(event);
+
+void CApp::OnKeyDown(SDL_Keycode &sym, Uint16 &mod)
+{
+    switch(sym)
+    {
+    case SDLK_UP:
+        GCamera::CameraControl.OnMove(0, 10);
+        break;
+    case SDLK_DOWN:
+        GCamera::CameraControl.OnMove(0, -10);
+        break;
+    case SDLK_LEFT:
+        GCamera::CameraControl.OnMove(10, 0);
+        break;
+    case SDLK_RIGHT:
+        GCamera::CameraControl.OnMove(-10, 0);
+        break;
+    default: {
+
+        }
+    }
+}
+
+
+void CApp::OnExit()
+{
+    running = false;
 }
