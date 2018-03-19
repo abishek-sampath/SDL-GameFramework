bcd202bc0ca4465914791fb6761c510a1b88ea7d
abisheksampath <sampath.ab@husky.neu.edu>
2018-03-19 01:42:56 -0400

    added area render code

--- CApp_OnRender.cpp
+++ CApp_OnRender.cpp
@@ -4,11 +4,14 @@
 
 void CApp::OnRender()
 {
-    for(unsigned int i=0; i < CEntity::entityList.size(); i++) {
-        if(!CEntity::entityList[i])
-            continue;
-        CEntity::entityList[i]->OnRender(texRectsMap[i+1]);
-    }
+//    for(unsigned int i=0; i < CEntity::entityList.size(); i++) {
+//        if(!CEntity::entityList[i])
+//            continue;
+//        CEntity::entityList[i]->OnRender(texRectsMap[i+1]);
+//    }
+
+    GArea::AreaControl.OnRender(GCamera::CameraControl.GetX(),
+                                GCamera::CameraControl.GetY());
 
     SDL_RenderPresent(renderer);
 }
