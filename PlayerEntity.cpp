a25ffa5fa499624af7953bd17dd0344caa7b5775
abisheksampath <sampath.ab@husky.neu.edu>
2018-03-21 02:08:31 -0400

    collison varialbles initialization, texture rect width and height

--- PlayerEntity.cpp
+++ PlayerEntity.cpp
@@ -2,13 +2,16 @@
 
 PlayerEntity::PlayerEntity(SDL_Renderer* renderer, ResourceManager* resourceManager) : GEntity(renderer, resourceManager)
 {
-
+    collisionX = 20;
+    collisionY = 0;
+    collisionWidth = 20;
+    collisionHeight = 0;
 }
 
 
-bool PlayerEntity::OnLoad(const char* file, int width, int height, int maxFrames)
+bool PlayerEntity::OnLoad(const char* file, int width, int height, int textureWidth, int textureHeight, int maxFrames)
 {
-    return GEntity::OnLoad(file, width, height, maxFrames);
+    return GEntity::OnLoad(file, width, height, textureWidth, textureHeight, maxFrames);
 }
 
 
@@ -58,5 +61,6 @@ void PlayerEntity::OnAnimate()
 bool PlayerEntity::OnCollision(GEntity* entity)
 {
     Jump();
+
     return true;
 }
