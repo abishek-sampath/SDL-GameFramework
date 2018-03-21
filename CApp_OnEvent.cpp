2aaa3e788563a35f07ccffb7408faf9638d78904
abisheksampath <sampath.ab@husky.neu.edu>
2018-03-21 02:09:24 -0400

    movement speed up control

--- CApp_OnEvent.cpp
+++ CApp_OnEvent.cpp
@@ -19,12 +19,21 @@ void CApp::OnKeyDown(SDL_Keycode &sym, Uint16 &mod)
     {
     case SDLK_LEFT:
         player1->moveLeft = true;
+        //GCamera::CameraControl.OnMove(-15, 0);
         break;
     case SDLK_RIGHT:
         player1->moveRight = true;
+        //GCamera::CameraControl.OnMove(15, 0);
+        break;
+    case SDLK_UP:
+        //GCamera::CameraControl.OnMove(0, -15);
+        break;
+    case SDLK_DOWN:
+        //GCamera::CameraControl.OnMove(0, 15);
         break;
     case SDLK_LSHIFT:
         player1->maxSpeedX = 20;
+        player1->maxSpeedY = 20;
         break;
     case SDLK_SPACE:
         player1->Jump();
@@ -46,6 +55,7 @@ void CApp::OnKeyUp(SDL_Keycode &sym, Uint16 &mod)
         break;
     case SDLK_LSHIFT:
         player1->maxSpeedX = 10;
+        player1->maxSpeedY = 1;
         break;
     default:{}
     }
